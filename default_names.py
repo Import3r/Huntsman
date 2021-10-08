8731bac55bda2945eb457dc99917a9b50b3c2bfa
Import3r <60158098+Import3r@users.noreply.github.com>
2021-10-08 06:34:05 +0300

    Restructure the code and some functions

--- default_names.py
+++ default_names.py
@@ -1,7 +1,6 @@
 TOOLS_DIR = 'installed_tools'
 BASE_DIR = 'huntsman_results'
 UNIQUE_SUB_FILE = 'unique-subdomains.all'
-RESOLV_SUB_FILE = 'resolvable-subdomains.all'
 SUB_GIT_FILE = 'subdomains.github'
 SUB_AMASS_FILE = 'subdomains.amass'
 AQUATONE_RES_DIR = 'aquatone_results'
