ba496bff0d14399be3a8e54ee43f1c0e46137fb7
Import3r <60158098+Import3r@users.noreply.github.com>
2021-10-19 19:03:36 +0300

    Move packages, wrapper functions to separate files in 'resources' along tools.json

---
+++ resources/packages.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+
+from sys import executable, path as here, argv as arg
+from subprocess import run, PIPE
+from subprocess import STDOUT, Popen as run_async
+from shutil import which
+from os import path, chmod, makedirs, mkdir, geteuid, getcwd, setpgrp, killpg, devnull
+import apt
+import git
+import wget
+import zipfile
+import requests
+import json
+import re
+import time
+import signal
