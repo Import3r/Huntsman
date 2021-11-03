8177a373827667ec3c1e92fbc17123cf9ee59b22
Import3r <60158098+Import3r@users.noreply.github.com>
2021-11-03 21:19:47 +0200

    Visual changes

--- resources/amass.py
+++ resources/amass.py
@@ -16,6 +16,7 @@ class Amass:
         self.exec_path = given_path
         self.output_file = path.join(RES_ROOT_DIR, self.output_file_name)
 
+
     def enumerator_proc(self, domains):
         target_domains = ','.join(domains)
         return run_async(f"{self.exec_path} enum --passive -d {target_domains} -nolocaldb", shell=True, stdout=PIPE, stderr=DEVNULL)
