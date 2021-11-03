e35b0b0cf025169576d8721920eeb7dcbf3d8e30
Import3r <60158098+Import3r@users.noreply.github.com>
2021-11-03 19:10:19 +0200

    Create a Class file for Amass

---
+++ resources/amass.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3
+
+from resources.packages import *
+
+class Amass:
+    install_type = "compiled"
+    exec_name = "amass"
+    output_file_name = "subdomains.amass"
+    remote_repo_name = "Amass"
+    zipfile_name = "amass.zip"
+    compiled_zip_url = "https://github.com/OWASP/Amass/releases/download/v3.13.4/amass_linux_amd64.zip"
+
+
+    def __init__(self, given_path) -> None:
+        self.exec_path = given_path
+
+
+    def enumerator_proc(self, domains):
+        target_domains = ','.join(domains)
+        return run_async(f"{self.exec_path} enum --passive -d {target_domains} -nolocaldb", shell=True, stdout=PIPE, stderr=DEVNULL)
