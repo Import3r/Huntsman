0a10b2325e676997ec9d8e3b76be7fed77409bd7
Import3r <60158098+Import3r@users.noreply.github.com>
2021-11-03 22:51:53 +0200

    Remove no longer needed variables from file

--- resources/static_names.py
+++ resources/static_names.py
@@ -1,23 +1,7 @@
 TOOLS_JSON_FILE = 'tools.json'
 PATHS_JSON_FILE = 'paths.json'
-
 HM_PKGS_DIR = 'resources'
 TOOLS_DIR = 'installed_tools'
-
 RES_ROOT_DIR = 'huntsman_results'
-AQUATONE_RES_DIR = 'aquatone_results'
-SBDZ_RES_DIR = 'subdomainizer_results'
-GOSPDR_RES_DIR = 'gospider_results'
-
 SUB_MASTER_FILE = 'subdomains.all'
-SUB_GIT_FILE = 'subdomains.github'
-SUB_AMASS_FILE = 'subdomains.amass'
-
 ENDP_MASTER_FILE = 'endpoints.all'
-ENDP_WAYBACK_FILE = 'endpoints.waybackurls'
-
-SBDZ_SUB_FILE = 'subdomains.subdomainizer'
-SBDZ_SECRET_FILE = 'secrets.subdomainizer'
-SBDZ_CLOUD_FILE = 'cloud-services.subdomainizer'
-
-GOSPDR_INPUT_FILE = 'subdom-urls.temp'
