3405219e4c147ffabbd858f3352eb1eebfe42817
Import3r <60158098+Import3r@users.noreply.github.com>
2021-11-03 19:13:00 +0200

    Create a Class file for Subdomainizer

---
+++ resources/subdomainizer.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+
+from resources.packages import *
+from resources.static_names import RES_ROOT_DIR
+
+class Subdomainizer:
+    install_type = "from_repo"
+    exec_name = "SubDomainizer.py"
+    req_file_name = "requirements.txt"
+    results_dir_name = "subdomainizer_results"
+    subs_file_name = "subdomains.subdomainizer"
+    secrets_file_name = "secrets.subdomainizer"
+    cloud_file_name = "cloud-services.subdomainizer"
+    remote_repo_name = "SubDomainizer"
+    remote_repo_url = "https://github.com/nsonaniya2010/SubDomainizer.git"
+
+
+    def __init__(self, given_path) -> None:
+        mkdir(path.join(RES_ROOT_DIR, self.results_dir_name))
+        self.exec_path = given_path
+        self.subs_loot_file = path.join(RES_ROOT_DIR, self.results_dir_name, self.subs_file_name)
+        self.secret_loot_file = path.join(RES_ROOT_DIR, self.results_dir_name, self.secrets_file_name) 
+        self.cloud_loot_file = path.join(RES_ROOT_DIR, self.results_dir_name, self.cloud_file_name)
+
+
+    def scraper_proc(self, subdoms_file):
+        return run_async(f"{self.exec_path} -k -l {subdoms_file} -o {self.subs_loot_file} -sop {self.secret_loot_file} -cop {self.cloud_loot_file}", shell=True)
