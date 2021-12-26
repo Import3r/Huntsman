b421812181e7e29eeb27da5a39230ef0c1bc9584
Import3r <60158098+Import3r@users.noreply.github.com>
2021-12-26 11:23:52 +0200

    Change some assets storage paths

--- packages/static_paths.py
+++ packages/static_paths.py
@@ -3,13 +3,13 @@
 from os import path
 from sys import argv
 
-HM_STORAGE_DIR_NAME = 'storage'
+HM_STORAGE_DIR_NAME = 'assets'
 HM_STORAGE_DIR = HM_STORAGE_DIR_NAME
 
-PATHS_JSON_FILE_NAME = '.inst_tools_paths.json'
 INST_TOOLS_DIR_NAME = 'installed_tools'
-PATHS_JSON_FILE = path.join(path.dirname(argv[0]), HM_STORAGE_DIR, PATHS_JSON_FILE_NAME)
+HM_WORDLISTS_DIR_NAME = 'wordlists'
 INST_TOOLS_DIR = path.join(HM_STORAGE_DIR, INST_TOOLS_DIR_NAME)
+HM_WORDLISTS_DIR = path.join(HM_STORAGE_DIR, HM_WORDLISTS_DIR_NAME)
 
 RES_ROOT_DIR_NAME = 'huntsman_results'
 RES_ROOT_DIR = RES_ROOT_DIR_NAME
@@ -24,5 +24,5 @@ ENDP_MASTER_FILE_NAME = 'endpoints.all'
 ENDP_HOUND_RES_DIR = path.join(RES_ROOT_DIR, ENDP_HOUND_RES_DIR_NAME)
 ENDP_MASTER_FILE = path.join(ENDP_HOUND_RES_DIR, ENDP_MASTER_FILE_NAME)
 
-HM_WORDLISTS_DIR_NAME = 'wordlists'
-HM_WORDLISTS_DIR = HM_WORDLISTS_DIR_NAME
+PATHS_JSON_FILE_NAME = '.inst_tools_paths.json'
+PATHS_JSON_FILE = path.join(path.dirname(argv[0]), PATHS_JSON_FILE_NAME)
