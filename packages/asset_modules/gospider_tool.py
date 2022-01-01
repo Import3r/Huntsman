adb22608860395c840456967230fe15ec2f9c5d8
Import3r <60158098+Import3r@users.noreply.github.com>
2022-01-01 15:49:49 +0200

    Change gospider output directory path

--- packages/asset_modules/gospider_tool.py
+++ packages/asset_modules/gospider_tool.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 
-from packages.static_paths import RES_ROOT_DIR, INST_TOOLS_DIR
+from packages.static_paths import ENDP_HOUND_RES_DIR, INST_TOOLS_DIR
 from packages.common_utils import store_results, text_from_set_of_lines
 from packages.install_handler import update_install_path
 from os import path, makedirs, rename
@@ -17,7 +17,7 @@ class GoSpider:
 
     def __init__(self, given_path) -> None:
         self.asset_path = given_path
-        self.output_dir = path.join(RES_ROOT_DIR, self.results_dir_name)
+        self.output_dir = path.join(ENDP_HOUND_RES_DIR, self.results_dir_name)
         self.input_file = path.join(self.output_dir, self.input_file_name)
         self.install_path = path.join(INST_TOOLS_DIR, self.remote_repo_name)
         self.output_buffer = ""
